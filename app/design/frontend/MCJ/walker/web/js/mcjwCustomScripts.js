79120c74b657a855504af7739d558d238302a934
ITCINFOTECH\23702 <vinay.jacob@itcinfotech.com>
2017-01-09 11:17:25 +0530

    footer links, search box styling. Added some important notes

--- app/design/frontend/MCJ/walker/web/js/mcjwCustomScripts.js
+++ app/design/frontend/MCJ/walker/web/js/mcjwCustomScripts.js
@@ -7,10 +7,10 @@ require([
 			
 			/*Search transition*/
 
-			$('input#search').mouseenter(function(){
+			$('input#search').onclick(function(){
 				$('input#search').css('width','100%');
 			});
-			$('input#search').mouseout(function(){
+			$('input#search').focus(function(){
 				if($('input#search').val().length>=1){
 					$('input#search').css('width','100%');
 				}
