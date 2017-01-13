9d008ab7ebd40374fd7c292179194c0e63394896
ITCINFOTECH\23702 <vinay.jacob@itcinfotech.com>
2017-01-13 13:00:48 +0530

    Env.php

---
+++ app/etc/env.php
@@ -0,0 +1,69 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_mcjw',
+  ),
+  'db' => 
+  array (
+    'connection' => 
+    array (
+      'indexer' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'mcjwedition1',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1',
+        'persistent' => NULL,
+      ),
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'mcjwedition1',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1',
+      ),
+    ),
+    'table_prefix' => '',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'ee0364520c7471d8c534afdf0377f5b7',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'target_rule' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Tue, 22 Nov 2016 10:46:51 +0000',
+  ),
+);
